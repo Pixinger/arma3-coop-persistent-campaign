f43126c6c1261a08d308ab865c56a0d0ae90e43a
pixinger <pixinger77@gmail.com>
2016-04-28 17:45:03 +0200

    Verwaltung

---
+++ arma3/TK_PersistentCampaign2.CreateLink.bat
@@ -0,0 +1,1 @@
+mklink /D "C:\Users\SST\Documents\Arma 3 - Other Profiles\[TK]Pix\missions\TK_PersistentCampaign2.Altis" "D:\_git_src\arma3-coop-persistent-campaign\arma3\TK_PersistentCampaign2.Altis"
---
+++ arma3/TK_PersistentCampaign3.Bornholm.CreatePbo.bat
@@ -0,0 +1,2 @@
+call "D:\_Programme\ExtractPbo\MakePbo" -A -N TK_PersistentCampaign3.Bornholm D:\[TK]PersistentCampaign3.Bornholm.pbo
+
